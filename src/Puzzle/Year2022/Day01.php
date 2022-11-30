4216afe0db16a17d37acbef10cc2e26745bfe127
Youri Wijnands <ywijnands@e-sites.nl>
2022-11-30 23:00:42 +0100

    Get ready ...

---
+++ src/Puzzle/Year2022/Day01.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Puzzle\Year2022;
+
+use App\Puzzle\AbstractPuzzle;
+
+class Day01 extends AbstractPuzzle
+{
+    public function part1(string $input): int
+    {
+        return 0;
+    }
+
+    public function part2(string $input): int
+    {
+        return 0;
+    }
+}
---
+++ tests/Year2022/Day01Test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Year2022;
+
+use App\Tests\AbstractPuzzleTestCase;
+
+class Day01Test extends AbstractPuzzleTestCase
+{
+    protected function setUp(): void
+    {
+        $this->solution = new \App\Puzzle\Year2022\Day01();
+        $this->data = file_get_contents(__DIR__.'/../../public/data/2022/01/example.txt'); // @phpstan-ignore-line
+    }
+
+    public function testPart1(): void
+    {
+        $this->assertSame(0, $this->solution->part1($this->data));
+    }
+
+    public function testPart2(): void
+    {
+        $this->assertSame(0, $this->solution->part2($this->data));
+    }
+}
